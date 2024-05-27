9423b3f0409bd95f581036b641397653e203981d
Letian Leng <113462284+T1mLen6@users.noreply.github.com>
2024-05-27 19:05:43 -0400

    rewrite json work finished

---
+++ after_translate_rearrange.py
@@ -0,0 +1,35 @@
+import json
+from collections import defaultdict
+import os
+
+# Read the data from the file
+with open("22.json", "r") as file:
+    lines = file.readlines()
+
+# Parse each line as a JSON object and group by FloorPlan
+floor_plan_data = defaultdict(list)
+
+for line in lines:
+    entry = json.loads(line)
+    floor_plan = entry.pop("FloorPlan") 
+    floor_plan_data[floor_plan].append(entry)
+
+
+# Define the target folder
+output_folder = "rearranged_taks_for_SmartLLM_2Task"
+
+# Create the target folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+
+# Write each group to a separate JSON file
+for floor_plan, entries in floor_plan_data.items():
+    filename = os.path.join(output_folder, f"{floor_plan}.json")
+    with open(filename, "w") as file:
+        for entry in entries:
+
+            file.write(json.dumps(entry) +'\n')
+
+
+
+print("Data has been split into separate files based on FloorPlan.")
--- alfred_2_smartllm.py
+++ alfred_2_smartllm.py
@@ -10,54 +10,46 @@ def transform_json(input_data, pattern):
     for entry in input_data:
         # Extract the task key and value
         task_keys = list(entry.keys())
-        print(entry)
+        robot_list = [1]
 
-        i = 0
-
-        for key in task_keys:
-
+        for i in range(3):
             if i == 0:
-                task_value = entry[key]
-
-                # Extract the floor plan from the task key
-                floor_plan = key.split('.')[0]
-
-            if i == 1:
-                # Extract the first initial position
+                robot_list = [1]
                 init_pos = entry["init_pos"][0][0]
 
+            if i == 1:
+                robot_list = [1,2]
+                init_pos = [entry["init_pos"][1][0], entry["init_pos"][1][1]]
+
             if i == 2:
+                robot_list = [1,2,3,4]
+                init_pos = [entry["init_pos"][2][0], entry["init_pos"][2][1], 
+                            entry["init_pos"][2][2], entry["init_pos"][2][3]]
+                
+            task_value = entry[task_keys[0]]
 
-                i = 0
+            # Extract the floor plan from the task key
+            floor_plan = task_keys[0].split('.')[0]
 
-            i += 1
+            # Construct the new dictionary
+            new_entry = {
+                "FloorPlan": floor_plan,
+                "task": task_value,
+                "robot list": robot_list,
+                "object_states": [],
+                "trans": 0,
+                "max_trans": 1,
+                "init_pos": init_pos
+            }
+            
+            # Append the new entry to the transformed data list
+            data_after.append(new_entry)
 
-        # Construct the new dictionary
-        new_entry = {
-            "FloorPlan": floor_plan,
-            "task": task_value,
-            "robot list": [1, 2, 3],
-            "object_states": [],
-            "trans": 0,
-            "max_trans": 1,
-            "init_pos": init_pos
-        }
-        
-        # Append the new entry to the transformed data list
-        data_after.append(new_entry)
-        #print(new_entry)
-    
     return data_after
 
 
-
-
-        
-        
-        
-
 # Load the JSON data from a file
-input_file = '/home/tim/alfred_multiagent/multiagent_tasks_1_summary_with_robot_init_pos.json'
+input_file = '/home/tim/alfred_multiagent/multiagent_tasks_2_summary_with_robot_init_pos.json'
 
 pattern = 'FloorPlan1|tasks_1|'
 
@@ -72,7 +64,7 @@ first_50_dict = input_json[:50]
 transformed_data = transform_json(first_50_dict, pattern)
 
 # Output the transformed data to a file
-with open('11.json', 'w') as f:
+with open('22.json', 'w') as f:
     for item in transformed_data:
         f.write(json.dumps(item) + '\n')
 
